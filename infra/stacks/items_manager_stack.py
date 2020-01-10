300f41dd000b34f53cb2f8d52e9043fee384c207
Alex Gelman <6887237+alexgelman@users.noreply.github.com>
2020-01-10 10:25:28 +0200

    make example shorter this way it'll be easier to uderstand at a glance

--- infra/stacks/items_manager_stack.py
+++ infra/stacks/items_manager_stack.py
@@ -12,31 +12,24 @@ class ItemsManagerStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        self.items_table = aws_dynamodb.Table(self, f"{id}ItemsTable",
-            partition_key=aws_dynamodb.Attribute(
+        partition_key = aws_dynamodb.Attribute(
                 name="name",
-                type=aws_dynamodb.AttributeType.STRING),
-            billing_mode=aws_dynamodb.BillingMode.PAY_PER_REQUEST,
-            removal_policy=core.RemovalPolicy.DESTROY)
+                type=aws_dynamodb.AttributeType.STRING)
+
+        self.items_table = aws_dynamodb.Table(self, f"{id}ItemsTable",
+            partition_key=partition_key)
 
         lambda_service_principal = aws_iam.ServicePrincipal('lambda.amazonaws.com')
-        self.api_handler_iam_role = aws_iam.Role(self, 'ItemsManagerApiHandlerLambdaRole', assumed_by=lambda_service_principal)
-
-        self.api_handler_iam_role.add_to_policy(aws_iam.PolicyStatement(
-            actions=[
-                "logs:CreateLogGroup",
-                "logs:CreateLogStream",
-                "logs:PutLogEvents"
-            ],
-            resources=["arn:aws:logs:*:*:*"],
-            effect=aws_iam.Effect.ALLOW))
-
+        self.api_handler_iam_role = aws_iam.Role(self, 'ApiHandlerLambdaRole',
+            assumed_by=lambda_service_principal)
         self.items_table.grant_read_write_data(self.api_handler_iam_role)
 
-        api_source_dir = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'api')
-
         chalice_stage_config = self._create_chalice_stage_config()
-        self.chalice_app = Chalice(self, 'ItemsManagerApi', source_dir=api_source_dir, stage_config=chalice_stage_config)
+        api_source_dir = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, 'api')
+        self.chalice_app = Chalice(self, 'ItemsManagerApi',
+            source_dir=api_source_dir,
+            stage_config=chalice_stage_config)
+
 
     def _create_chalice_stage_config(self):
         chalice_stage_config = {
